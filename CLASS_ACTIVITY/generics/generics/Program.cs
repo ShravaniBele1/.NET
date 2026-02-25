4aa686ddac300609514b56c30a3c78bad4dc8c43
Shravani Bele <shravanibele1008@gmail.com>
2026-02-25 17:49:16 +0530

    Generics added

---
+++ CLASS_ACTIVITY/generics/generics/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList List = new ArrayList();
+            List.Add(10);
+            List.Add(20);
+            List.Add("C#"); //this is not typesafety.
+
+            //generics
+            List<int> num = new List<int>();
+            num.Add(10);
+            num.Add(20);
+            // num.Add("#"); //it is not right 
+
+            myClass<int> obj1 = new myClass<int>();
+            obj1.Data = 20;
+
+            myClass<String> obj2 = new myClass<String>();
+            obj2.Data = "Shravani";
+        }
+    }
+     class myClass<T>
+     {     
+        public T Data;
+        public void Show()
+        {
+            Console.WriteLine(Data);
+        }
+     }
+ }
+
+
+//generics allows us to define classes ,methods and collections with a placeholder for a datatype so they work with any type while maintaining type safety.
+
+//activity 
+//write 2 ex with generics
+//realtime use of generics in company
+//why comapny uses generics(typesafety)
+
+
+//lamda expression is short way of writing anonomous method using arrow operator
+//(parameter) => expression 
+//activity -
+//write 2 examples of lymbda expression 
+//where we use it in company
+//create a generic class to store two values
+//what generics?
+//why generics are better than arraylist?
+//what is lymda expression?
+//diff between deligates and lymda?
+
+
+//Synchronous AND Asynchronous  
---
+++ LAB_ACTIVITY/Lab04/Multiclass_Deligate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Multicast_Deligate
+{
+    // Step 1: Create a delegate
+    public delegate void MyDelegate();
+
+    static void Message1()
+    {
+        Console.WriteLine("Hello");
+    }
+
+    static void Message2()
+    {
+        Console.WriteLine("Welcome");
+    }
+
+    static void Message3()
+    {
+        Console.WriteLine("To C# Multicast Delegate");
+    }
+
+    static void Main()
+    {
+        // Step 2: Create delegate object
+        MyDelegate obj;
+
+        // Step 3: Add methods
+        obj = Message1;
+        obj += Message2;
+        obj += Message3;
+
+        // Step 4: Call delegate
+        obj();
+    }
+}
